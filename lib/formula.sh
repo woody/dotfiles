fd76fe15a1ea72d7399807f580d3890cf31d2e9d
Woody Ai <han.ai@me.com>
2015-01-29 17:19:16 +0800

    Declare homebrew formulas with array.

--- lib/formula.sh
+++ lib/formula.sh
@@ -1,89 +1,87 @@
 #!/usr/bin/env bash
-# Homebrew formula
+# Managed Mac packages and application binaries by homebrew
 
 set -e
 set -u
 
 source $DOTFILES_LOCAL/lib/prompt.sh
 
-require_brew () {
-  # homebrew installed?
-  { /usr/bin/which brew >/dev/null; } || {
+# Export formulas declaration
+[[ -e $DOTFILES_LOCAL/formulas ]] && {
+  source $DOTFILES_LOCAL/formulas
+}
 
-    { # install homebrew
-      $(/usr/bin/which ruby) -e \
-      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null
-    } || {
-      # prompt install homebrew fails
-      error "Install homebrew."
-      exit
-    }
+
+# Require homebrew install packages
+{ /usr/bin/which brew >/dev/null; } || {
+
+  { # install homebrew
+    $(/usr/bin/which ruby) -e \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null
+  } || {
+    # prompt install homebrew fails
+    error "Install homebrew."
+    exit
   }
 }
 
-require_cask () {
-  # cask is homebrew for Mac application
-  [[ -x $(brew --prefix)/bin/brew-cask ]] || {
-    # install brew-cask
-    { brew install caskroom/cask/brew-cask >/dev/null; } || {
-      # prompt brew-cask install fail
-      error "Install brew-cask"
-      exit
-    }
+
+# require brew-cask install Mac application
+[[ -x $(brew --prefix)/bin/brew-cask ]] || {
+  # install brew-cask
+  { brew install caskroom/cask/brew-cask >/dev/null; } || {
+    # prompt brew-cask install fail
+    error "Install brew-cask"
+    exit
   }
 }
 
-tap () {
-  # Tap extend homebrew external formula repo
-  require_brew
 
-  # taps function allowed multiple arguments
-  for t in "$@"; do
+set +u
+
+# extend homebrew external formula repo defined $taps array
+[[ $taps ]] && {
+  for t in "${taps[@]}"; do
+    # Already tapped?
     { brew tap | /usr/bin/grep "$t" >/dev/null; } || {
-      { # tap external formula repo
-        echo "Tapping $t..."
-        brew tap "$t" 2>/dev/null
-      } && {
-        success "Tap $t"
-      } || {
-        # tap fail and prompt error
+      echo "Tapping $t..."
+      { brew tap "$t" 2>/dev/null; } || {
+        # prompt tap fail
         error "Tap $t"
+        false
       }
+    } && {
+      # prompt tap success
+      success "Tap $t"
     }
   done
-
 }
 
-formula () {
-  # Declare homebrew formula
-  # Require homebrew installed
-  require_brew
-
-  for f in "$@"; do
-    # Formula installation options following formula name.
+# install formulas defined by $formulas array
+[[ $formulas ]] && {
+  for f in "${formulas[@]}"; do
+    # formula name could followed by installation options
     # Extract formula name
-    local name=$(/bin/echo $f | /usr/bin/awk '{print $1}')
+    n=$(/bin/echo $f | /usr/bin/awk '{print $1}')
     # Formula is installed?
-    { $(/usr/bin/which brew) list | /usr/bin/grep "$name" >/dev/null; } || {
+    { brew list | /usr/bin/grep "$n" >/dev/null; } || {
       # Install formula
-      echo "Installing $name..."
-      { $(/usr/bin/which brew) install "$f" >/dev/null; } || {
+      echo "Installing $n..."
+      { brew install "$f" >/dev/null; } || {
         # prompt formula install fails
-        error "Install $name."
+        error "Install $n"
         false
       }
     } && {
       # prompt formula install success
-      success "Install $name."
+      success "Install $n"
     }
   done
 }
 
-cask () {
-  # Declare homebrew cask
-  require_cask
-
-  for c in "$@"; do
+# install cask defined by $casks array
+[[ $casks ]] && {
+    for c in "${casks[@]}"; do
     # cask installed?
     { brew cask list 2>/dev/null | grep "$c" >/dev/null; } || {
       # Install cask
@@ -98,15 +96,4 @@ cask () {
       success "Install $c"
     }
   done
-}
-
-
-tap "caskroom/cask" \
-    "homebrew/binary" \
-    "homebrew/completions" \
-    "homebrew/dupes"
-
-
-[[ -e $DOTFILES_LOCAL/formulas ]] && {
-  source $DOTFILES_LOCAL/formulas
 }
