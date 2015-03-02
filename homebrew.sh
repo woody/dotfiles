51b957d1c5f1f8a9dc4a1637fb8c5dbe09d56a15
Woody Ai <han.ai@me.com>
2015-03-02 22:26:50 +0800

    [Fixed] Export homebrew.sh cause shell exit.

--- homebrew.sh
+++ homebrew.sh
@@ -4,8 +4,6 @@
 set -e
 set -u
 
-declare -a successIssues
-declare -a failIssues
 
 reportSuccess () {
   local successIssuesCount=${#successIssues[@]}
@@ -20,7 +18,7 @@ reportFail () {
 # I think this should be call at top
 # make sure /usr/local/bin at PATH for installing homebrew
 { ruby -e 'abort unless ENV["PATH"].split(":").include? "/usr/local/bin"'; } \
-|| epxort PATH=/usr/local/bin:$PATH
+|| export PATH=/usr/local/bin:$PATH
 
 # homebrew manage OSX missing packages
 # homebrew installed?
@@ -34,11 +32,3 @@ reportFail () {
 } && {
   reportSuccess homebrew
 }
-
-for success in #{successIssues[@]}; do
-  echo "=> $success"
-done
-
-for fail in #{failIssues[@]}; do
-  echo "<= $fail"
-done
