d3a8d22b45d965bc698dc0eb11676dd196fa5770
Woody Ai <han.ai@me.com>
2015-04-10 11:21:23 +0800

    Remove from $PATH.

--- path.sh
+++ path.sh
@@ -5,7 +5,7 @@ export PATH_INCLUDED=true
 # Remove an entry from $PATH
 # Based on http://stackoverflow.com/a/2108540/142339
 # Steal from cowboy/dotfiles
-function path_remove() {
+function remove_path() {
   local arg path
   path=":$PATH:"
   for arg in "$@"; do path="${path//:$arg:/:}"; done
